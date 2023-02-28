157d2472a56adf5422e1e8b7a81d5fe7639142f0
Daniel <kronkowski.daniel@gmail.com>
2023-02-28 21:07:22 +0100

    post method

--- app.py
+++ app.py
@@ -1,15 +1,51 @@
 import os
-
+import certifi
+import ssl
 from flask import Flask, jsonify, Response, request, render_template
+import geopy.geocoders
+from geopy.geocoders import Nominatim
 from db_manager import DatabaseManager
 
 app = Flask(__name__)
+
+ctx = ssl.create_default_context(cafile=certifi.where())
+geopy.geocoders.options.default_ssl_context = ctx
+geolocator = Nominatim(user_agent="z2j_map")
+
+def get_location_from_city(city):
+    location = geolocator.geocode(city)
+    return location.latitude, location.longitude
+
+def postcode_to_city(zip: str):
+    postcode = zip
+    location = geolocator.geocode(postcode, addressdetails=True)
+
+    city_name = None
+    if location.raw["address"].get("city"):
+        city_name = location.raw["address"]["city"]
+    elif location.raw["address"].get("town"):
+        city_name = location.raw["address"]["town"]
+    elif location.raw["address"].get("village"):
+        city_name = location.raw["address"]["village"]
+
+    return city_name
 
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
 
 # POST endpoint
+@app.route('/users', methods=['POST'])
+def add_user():
+    db = DatabaseManager(database=os.getenv('database'), user=os.getenv('user'), password=os.getenv('password'),
+                         host=os.getenv('host'))
+    request_data = request.get_json()
+    username, zip, field = request_data["discord"], request_data["zip"], request_data["stack"]
+    city_name = postcode_to_city(zip)
+    latitude, longitude = get_location_from_city(city_name)[0], get_location_from_city(city_name)[1]
+    result = Response(db.add_user(username, city_name, field, latitude, longitude), mimetype="application/json")
+    db.close_connection() 
+    return result, 201
 
 
 # GET endpoint
@@ -33,13 +69,14 @@ def update_users(discord):
         updated_fields.append('new_discord')
         db.edit_user_name(discord, data['new_discord'])
 
-    if 'city_name' in data:
-        updated_fields.append('city_name')
-        db.edit_user_city(discord, data['city_name'])
+    '''if 'zip' in data:
+        updated_fields.append('zip')
+        db.edit_user_city(discord, data['zip'])'''
+        
 
     if 'stack' in data:
         updated_fields.append('stack')
-        db.edit_user_stack(discord, data['stack'])
+        return db.edit_user_stack(discord, data['stack'])
 
     if len(updated_fields) > 0:
         response = jsonify({"message": f"Updated fields: {', '.join(updated_fields)}"}), 200
